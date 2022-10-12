35e6794c6b91cbe5d3e18be3ac0e056c032a9c88
JVSBerkay <114753405+JVSBerkay@users.noreply.github.com>
2022-10-12 18:19:16 +0300

    Add files via upload

---
+++ vize final odevi deneme2/vize final odevi deneme2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace vize_final_odevi_deneme2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(102, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vize";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(98, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Final";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 51);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(95, 26);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.button1_Click);
+            this.numericUpDown1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(152, 94);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(95, 26);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.button1_Click);
+            this.numericUpDown2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sonuc= ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(406, 264);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Vize Final Hesaplama";
+            this.Load += new System.EventHandler(this.form1_load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ vize final odevi deneme2/vize final odevi deneme2/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vize_final_odevi_deneme2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double vize, final, final2, ort;
+
+        private void button1_Click(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void form1_load(object sender, EventArgs e)
+        {
+            label3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double.TryParse(numericUpDown1.Text, out vize);
+
+            double.TryParse(numericUpDown2.Text, out final);
+
+
+
+            ort = vize * 0.4 + final * 0.6;
+
+
+            if (final < 50)
+
+            {
+
+                label3.Text = "Ortalamanız: " + ort + " Harf Notunuz: FF";
+
+                label3.ForeColor = Color.Red;
+
+                MessageBox.Show("(Final) sinavinda baraji gecemediginiz icin Kaldiniz !! :(");
+
+            }
+
+
+            else
+
+            {
+
+                if (ort >= 90 && ort <= 100)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " AA (Gectiniz !!) :) ";
+
+                    label3.ForeColor = Color.Green;
+
+                }
+
+                else if (ort >= 80 && ort < 90)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " BA (Gectiniz !!) :) ";
+
+                    label3.ForeColor = Color.Green;
+
+                }
+
+                else if (ort >= 70 && ort < 80)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " BB (Gectiniz !!) :) ";
+
+                    label3.ForeColor = Color.Green;
+
+                }
+
+                else if (ort >= 60 && ort < 70)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " CB (Gectiniz !!) :) ";
+
+                    label3.ForeColor = Color.Green;
+
+                }
+
+                else if (ort >= 50 && ort < 60)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " CC (Gectiniz !!) :) ";
+
+                    label3.ForeColor = Color.Green;
+
+                }
+
+                else if (ort >= 45 && ort < 50)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " DD (Kaldiniz !) :( ";
+
+                    label3.ForeColor = Color.Red;
+
+                }
+
+                else if (ort >= 0 && ort < 45)
+
+                {
+
+                    label3.Text = "Ortalamanız: " + ort + " FF (Kaldiniz !) :( ";
+
+                    label3.ForeColor = Color.Red;
+
+
+                }
+
+            
+
+            }
+
+        }
+
+
+    }
+
+   
+}
